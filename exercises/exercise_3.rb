31f6dcfb83baa8c99a9ccc05694335cc60b3be06
Aaron Timbol <timbolaj@gmail.com>
2020-06-22 18:54:30 +0000

    finished exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,12 @@
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
-
+# bundle exec ruby exercises/exercise_3.rb
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts Store.count
