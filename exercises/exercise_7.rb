291dccfeaad71cd77b92bf480c122d99c3b5885f
Aaron Timbol <timbolaj@gmail.com>
2020-06-22 20:28:08 +0000

    Finished exercise with stretch tasks

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please type in the store name"
+@store_name = gets.chomp
+new_store = Store.create(name: @store_name)
+
+if new_store.errors.messages
+  new_store.errors.messages.each do |error_key, message|
+    puts "Error: #{message.first}"
+  end
+end
